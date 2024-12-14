ed7758ce78621ec47d3a8607a68cc7fd43dcb565
subhasrath <subhas.rath15@gmail.com>
2024-12-15 02:04:08 +0530

    commited on 15/12/2024

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
-
+  firstName:String = "Angular Tutorial";
+  angularVersion = "Version 17";
+  version:number =17;
+  isActive:boolean = false;
+  currerntDate:Date | undefined;
+  inputType:string = "radio";
+  getCurrentDate(){
+    this.currerntDate = new Date();
+  }
 }
