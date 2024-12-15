8e3355e8e7943fbf16b96b2897fcee07d14d3079
subhasrath <subhas.rath15@gmail.com>
2024-12-16 01:05:19 +0530

    commiting on 16/12/2024

---
+++ src/app/Interfaces/Roles.ts
@@ -0,0 +1,4 @@
+export interface IRoles{
+    roleId: number,
+    role: string
+}
--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import {IRoles} from '../../Interfaces/Roles';
 @Component({
   selector: 'app-roles',
   standalone: true,
@@ -7,14 +9,27 @@ import { CommonModule } from '@angular/common';
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
+export class RolesComponent implements OnInit{
   firstName:String = "Angular Tutorial";
   angularVersion = "Version 17";
   version:number =17;
   isActive:boolean = false;
   currerntDate:Date | undefined;
   inputType:string = "radio";
+  roleList: IRoles []= [];
+  http = inject(HttpClient);
+  
+  ngOnInit(): void {
+    this.getAllRoles();
+  }
+  
   getCurrentDate(){
     this.currerntDate = new Date();
   }
+  getAllRoles(){
+    this.http.get("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles").subscribe((res:any)=>{
+      this.roleList = res;
+    })
+  }
+  
 }
